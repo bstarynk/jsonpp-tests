3670fb97997c4ca0a3d67e77ce2fc7ea661d98d2
Basile Starynkevitch <basile@starynkevitch.net>
2013-11-08 14:10:47 +0100

    using closures, but should use also pointers to Json values...

--- test-jsoncpp.cc
+++ test-jsoncpp.cc
@@ -7,15 +7,18 @@
 // test the jsoncpp library from http://jsoncpp.sourceforge.net/
 // coded in C++11
 
-#include <json/json.h>
 #include <string>
 #include <fstream>
 #include <random>
+#include <functional>
+#include <list>
+#include <vector>
 #include <cassert>
 #include <climits>
 #include <argp.h>
 #include <time.h>
 #include <string.h>
+#include <json/json.h>
 
 
 std::ranlux24* my_randomgen;
@@ -93,34 +96,84 @@ public:
   ~Myargs() {};
 };
 
+std::string
+my_random_attr(unsigned sz)
+{
+  static const char* atstrings[] = {
+    "zero", "one", "two", "three", "four", "five", "six", "seven",
+    "eight", "nine", "ten", "blue", "green", "yellow", "red",
+    "pink", "black", "sad", "happy", "big", "tiny", "left", "right"
+  };
+  const unsigned nbat = sizeof(atstrings)/sizeof(atstrings[0]);
+  assert (sz>2);
+  unsigned r = my_random() % sz;
+  if (r<nbat) return std::string(atstrings[r]);
+  else {
+    std::string s="_";
+    while (r>0) {
+      s.push_back('A' + r % 26);
+      r = r/26;
+    };
+    return s;
+  }
+}
+
+typedef std::function<bool(const Json::Value&)> action_t;
+
+bool
+do_random_action(std::vector<action_t>& vectact, const Json::Value&jv)
+{
+  unsigned width = vectact.size();
+  for (unsigned cnt = width/4 + (my_random() % (width/2+1));
+       cnt > 0;
+       cnt--)
+  { auto f = vectact[my_random() % width];
+    if (f(jv)) return true;
+  }
+  return false;
+}
+
 Json::Value
 my_random_json(unsigned sz, unsigned width)
 {
+  assert(my_randomgen != nullptr);
   assert (sz < INT_MAX);
   assert (width < SHRT_MAX && width >= 2);
-  static const char* atstrings[] = {
-    "one", "two", "three", "four", "five", "six", "seven",
-    "eight", "nine", "ten", "blue", "green", "yellow", "red",
-    "pink", "black", "sad", "happy", "big", "tiny"
-  };
   Json::Value jroot(Json::objectValue);
-  std::vector<Json::Value> jvect;
-  jvect.resize(width);
-  assert(my_randomgen != nullptr);
+  // an action may change a value and return true, or do nothing and return false
+#warning we should also keep the pointer on which we are doing the action...
+  std::vector<action_t> actionvect;
+  actionvect.reserve(width);
   /// build a random jroot JSON object
-  jroot["build"] = __DATE__;
-  jroot["size"] = sz;
-  jroot["random"] = my_random() % 100000;
-  while (sz-- > 0) {
-    unsigned r = my_random() % 7;
+  jroot["@build"] = __DATE__"-" __TIME__;
+  jroot["@size"] = sz;
+  jroot["@width"] = width;
+  jroot["@random"] = my_random() % 100000;
+  // initialize the action vector to fill the root
+  for (unsigned cnt=0; cnt<width; cnt++) {
+    auto at = my_random_attr((5*width/4+1)|7);
+    jroot[at] = Json::Value::null;
+    actionvect.push_back([=,&jroot](const Json::Value& jv)
+    { if (jroot[at].isNull())
+      {
+        jroot[at]=jv;
+        return true;
+      }
+      else return false;
+    });
+  }
+  while (sz > 0) {
+    unsigned r = my_random() % 9;
     Json::Value jv;
-    bool composite = false;
     DEBUG("sz=" << sz << " r=" << r);
     switch (r) {
-    case 0:
-      jv = Json::Value((int)my_random() % 10000);
+    case 0: // random number
+      jv = Json::Value((int)my_random() % (10000+5*width));
+      DEBUG("number jv=" << jv);
+      if (do_random_action(actionvect,jv))
+        sz--;
       break;
-    case 1:
+    case 1: // random small string
     {
       char buf[4] = {0,0,0,0};
       buf[0] = 'a' + (my_random() % 26);
@@ -128,62 +181,101 @@ my_random_json(unsigned sz, unsigned width)
       if (my_random() % 3)
         buf[2] = '0' + (my_random() % 10);
       jv = Json::Value(buf);
+      DEBUG("string jv=" << jv);
+      if (do_random_action(actionvect,jv))
+        sz--;
     }
     break;
-    case 2:
+    case 2: // bigger random string, attribute-like
+    {
+      char nbuf[24];
+      memset (nbuf, 0, sizeof(nbuf));
+      snprintf (nbuf, sizeof(nbuf), "+%u", sz);
+      std::string sv = "_" + my_random_attr(width+4) + nbuf;
+      jv = Json::Value(sv);
+      DEBUG("nice string jv=" << jv);
+      if (do_random_action(actionvect,jv))
+        sz--;
+    }
+    break;
+    // more often, a random object
     case 3:
-      jv = Json::Value(Json::objectValue);
-      jv["num"] = Json::Value (Json::Int64(-((long)sz+1)));
-      composite = true;
-      break;
     case 4:
     case 5:
+    {
+      jv = Json::Value(Json::objectValue);
+      jv["@num"] = Json::Value (Json::Int64(-((long)sz+1)));
+      DEBUG("object jv=" << jv);
+      std::list<std::pair<unsigned,action_t>> actlist;
+      for (unsigned cnt = width/2 + 1 + my_random() % (width/4+1);
+           cnt > 0;
+           cnt--) {
+        auto at = my_random_attr(4*width/3+1);
+        jv[at] = Json::Value::null;
+        actlist.push_back( {my_random() % width,
+                            [=,&jv](const Json::Value& jcomp)
+        { if (jv[at].isNull())
+          {
+            jv[at]=jcomp;
+            DEBUG("objaction at=" << at << "; jv=" << jv);
+            return true;
+          }
+          else return false;
+        }
+                           });
+      }
+      if (do_random_action(actionvect,jv))
+        sz--;
+      for (auto pr : actlist)
+      {
+        unsigned ix = pr.first;
+        action_t act = pr.second;
+        actionvect[ix] = act;
+      };
+    }
+    break;
+    // quite often, an array
+    case 6:
+    case 7:
     {
       char buf [32];
       memset (buf, 0, sizeof(buf));
       snprintf(buf, sizeof(buf), "_%d_", sz);
       jv = Json::Value(Json::arrayValue);
       jv.append(Json::Value(buf));
-      composite = true;
+      DEBUG("array jv=" << jv);
+      std::list<std::pair<unsigned,action_t>> actlist;
+      for (unsigned cnt = width/2 + 1 + my_random() % (width/4+1);
+           cnt > 0;
+           cnt--) {
+        actlist.push_back( {my_random() % width,
+                            [=,&jv](const Json::Value& jcomp)
+        {
+          jv.append(jcomp);
+          DEBUG("array action jv=" << jv);
+          return true;
+        }
+                           });
+      }
+      if (do_random_action(actionvect,jv))
+        sz--;
+      for (auto pr : actlist)
+      {
+        unsigned ix = pr.first;
+        action_t act = pr.second;
+        actionvect[ix] = act;
+      };
     }
     break;
-    case 6:
+    // perhaps a boolean or a null
+    case 8:
       if (my_random() % 2)
         jv = Json::Value((bool) (my_random() % 2));
+      DEBUG("atomic jv=" << jv);
+      if (do_random_action(actionvect,jv))
+        sz--;
       break;
     }
-    DEBUG("jv=" << jv);
-    unsigned rix = (my_random())%width;
-    Json::Value &jcont = jvect[rix];
-    DEBUG("rix=" << rix << " old jcont=" << jcont);
-    if (jcont.isArray())
-      jcont.append(jv);
-    else if (jcont.isObject())
-    {
-      char atbuf[8];
-      snprintf(atbuf, sizeof(atbuf),"_%c%c%d",
-               (int) ('A' + my_random() % 26),
-               (int) ('A' + my_random() % 26),
-               (int) my_random() % 100);
-      jcont[atbuf] = jv;
-    }
-    else if (jcont.isNull()) {
-      std::string ats = atstrings[my_random()
-                                  % (sizeof(atstrings)/sizeof(atstrings[0]))];
-      if (!jroot.isMember(ats))
-        jroot[ats] = jv;
-      DEBUG("ats=" << ats << " jv=" << jv);
-    };
-    if (composite) {
-      std::string atr = atstrings[my_random()
-                                  % (sizeof(atstrings)/sizeof(atstrings[0]))];
-      if (!jroot.isMember(atr))
-        jroot[atr] = jv;
-      else if (jvect[rix].isNull() || my_random() % (4*width/3+1) == 0)
-        jvect[rix] = jv;
-    }
-    if (my_random() % (2*width+1) == 0)
-      jvect[my_random () % width] = Json::Value::null;
   }
   return jroot;
 }
